93e3a6a378f6aead59f8c057e164b7b7d4ba3bb6
thedly <thedly@taxbackinternational.com>
2020-08-17 18:38:18 +0530

    Updated the css for timeline

--- js/helpers.js
+++ js/helpers.js
@@ -1,4 +1,10 @@
-$(function(){
+$(function() {
 
-    
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > $(window).height()) {
+            $('.move-up').fadeIn();
+        } else {
+            $('.move-up').fadeOut();
+        }
+    });
 })
