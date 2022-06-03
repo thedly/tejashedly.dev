255197a6c719af9535b6a24335a2b8a534a9a2de
Tejas Hedly <tejas.hedly@kochgs.com>
2022-06-03 14:24:52 +0530

    Added switch to change theme

--- js/helpers.js
+++ js/helpers.js
@@ -1,10 +1,14 @@
-$(function() {
-
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > $(window).height()) {
-            $('.move-up').fadeIn();
-        } else {
-            $('.move-up').fadeOut();
+$(function(){
+    $(".switch #switch-theme").change(function(){
+        if($(this).prop("checked")){
+            $("body").css({"background-color": "rgba(255,255,255, 0.3)"})
+            $(".content .profile-header h1,.content .profile-header .sub-heading, .content .learn p").css({"color": "#000000"})
         }
-    });
+        else
+        {
+            $("body").css({"background-color": "rgba(0,27,58, 0.7)"})
+            $(".content .profile-header h1,.content .profile-header .sub-heading, .content .learn p").css({"color": "#FFFFFF"})
+        }
+    })
 })
+
