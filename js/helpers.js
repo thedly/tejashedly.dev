6c4f5ad93aea71787b3f8380a0593d844b1f6605
Tejas Hedly <tejas.hedly@kochgs.com>
2022-06-03 18:31:19 +0530

    Added localstorage implementation

--- js/helpers.js
+++ js/helpers.js
@@ -1,14 +1,34 @@
 $(function(){
+
+    var theme = localStorage.getItem("theme");
+    if(theme == "light") {
+        $(".switch #switch-theme").prop("checked", true)
+        set_light()
+    }
+    else {
+        $(".switch #switch-theme").prop("checked", false)
+        set_dark()
+    }
+
     $(".switch #switch-theme").change(function(){
         if($(this).prop("checked")){
-            $("body").css({"background-color": "rgba(255,255,255, 0.3)"})
-            $(".content .profile-header h1,.content .profile-header .sub-heading, .content .learn p").css({"color": "#000000"})
+            set_light()
+            localStorage.setItem("theme", "light");
         }
         else
         {
-            $("body").css({"background-color": "rgba(0,27,58, 0.7)"})
-            $(".content .profile-header h1,.content .profile-header .sub-heading, .content .learn p").css({"color": "#FFFFFF"})
+            set_dark()
+            localStorage.setItem("theme", "dark");
         }
     })
 })
 
+function set_dark() {
+    $("body").css({"background-color": "rgba(0,27,58, 0.7)"})
+    $(".content .profile-header h1,.content .profile-header .sub-heading, .content .learn p").css({"color": "#FFFFFF"})
+}
+
+function set_light(){
+    $("body").css({"background-color": "rgba(255,255,255, 0.3)"})
+    $(".content .profile-header h1,.content .profile-header .sub-heading, .content .learn p").css({"color": "#000000"})
+}
