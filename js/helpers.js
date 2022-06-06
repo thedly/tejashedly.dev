72494138c5c6800661b9e4fb6f2685c329d21310
Tejas Hedly <tejas.hedly@kochgs.com>
2022-06-06 10:49:49 +0530

    Updated UI & readme

--- js/helpers.js
+++ js/helpers.js
@@ -2,35 +2,31 @@ $(function(){
 
     var theme = localStorage.getItem("theme");
     if(theme == "light") {
-        $(".switch #switch-theme").prop("checked", true)
+        $(".switch #switch-theme").prop("checked", false)
         set_light()
     }
     else {
-        $(".switch #switch-theme").prop("checked", false)
+        $(".switch #switch-theme").prop("checked", true)
         set_dark()
     }
 
     $(".switch #switch-theme").change(function(){
         if($(this).prop("checked")){
-            set_light()
-            localStorage.setItem("theme", "light");
+            set_dark()
+            localStorage.setItem("theme", "dark");
         }
         else
         {
-            set_dark()
-            localStorage.setItem("theme", "dark");
+            set_light()
+            localStorage.setItem("theme", "light");
         }
     })
 })
 
 function set_dark() {
-    $("body").css({"background-color": "rgba(0,27,58, 0.7)"})
-    $(".content .profile-header h1,.content .profile-header .sub-heading, .content .learn p").css({"color": "#FFFFFF"})
-    $(".navbar .logo").addClass("on")
+    $("body, .navbar .logo, .navbar .logo img, .content .profile-header h1,.content .profile-header .sub-heading, .content .learn p, .content .resume-text, .copyrights, .navbar .logo-wrap").addClass("on")
 }
 
 function set_light(){
-    $("body").css({"background-color": "rgba(255,255,255, 0.3)"})
-    $(".content .profile-header h1,.content .profile-header .sub-heading, .content .learn p").css({"color": "#000000"})
-    $(".navbar .logo").removeClass("on")
+    $("body, .navbar .logo, .navbar .logo img, .content .profile-header h1,.content .profile-header .sub-heading, .content .learn p,.content .resume-text, .copyrights, .navbar .logo-wrap").removeClass("on")
 }
